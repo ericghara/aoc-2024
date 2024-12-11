8d44f8ae34c6514bbb68658663d96989e487ab17
ericghara <eric.gharakhanian@gmail.com>
2024-12-11 02:08:47 -0800

    day 11 tweaks

--- 11/sol_11.go
+++ 11/sol_11.go
@@ -4,44 +4,45 @@ import (
     "aoc/parseutil"
     "log"
     "strings"
-    "fmt"
     "strconv"
 )
 
-
 func main() {
-    
     input := parseutil.ParseInts(strings.Split(parseutil.ReadInputLines()[0], " "))
+    memo := map[[2]int64]int64{}
 
-    simulate := func(nums []int64, rounds int) []int64 {
-            last := nums
-            for ;rounds > 0; rounds-- {
-                next := []int64{}
-                for _, n := range last {
-                    if n == 0 {
-                        next = append(next, 1)
-                    } else if numStr := fmt.Sprint(n); len(numStr) % 2 == 0 {
-                        a, err0 := strconv.ParseInt(numStr[:len(numStr)/2], 10, 64)
-                        b, err1 := strconv.ParseInt(numStr[len(numStr)/2:], 10, 64)
-                        if (err0 != nil || err1 != nil) {
-                            log.Println("HMM")
-                        }
-                        next = append(next, a, b)
-                    } else {
-                        next = append(next, n * 2024)
-                        if n*2024 < 0 {
-                            log.Panic("OVERFLOW")
-                        }
-                    }
-                }
-                last = next
-            }
-            return last
+    var simulate func(int64, int64, int64) int64
+
+    simulate = func(num, round, endRound int64) int64 {
+        roundLeft := endRound-round
+        if roundLeft == 0 {
+            return 1
         }
+        if stones, ok := memo[[2]int64{num,roundLeft}]; ok {
+            return stones
+        }
+        var numStones int64
+        if num == 0 {
+            numStones = simulate(1, round+1, endRound)
+        } else if numStr := strconv.FormatInt(num, 10); len(numStr) % 2 == 0 {
+            a, _ := strconv.ParseInt(numStr[:len(numStr)/2], 10, 64)
+            b, _ := strconv.ParseInt(numStr[len(numStr)/2:], 10, 64)
+            numStones = simulate(a, round+1, endRound) + simulate(b, round+1, endRound)
+        } else {
+            numStones = simulate(num * 2024, round+1, endRound)
+        }
+        memo[[2]int64{num,roundLeft}] = numStones
+        return numStones
+    }
 
-    endState := simulate(input, 25)
-    //log.Println(endState)
-    log.Println("Num Stones", len(endState))
+    var totStones1, totStones2, rounds1, rounds2 int64
+    rounds1, rounds2 = 25, 75
 
+    for _, num := range input {
+        totStones1 += simulate(num, 0, rounds1)
+        totStones2 += simulate(num, 0, rounds2)
+    }
 
+    log.Println("pt1 Num Stones:", totStones1)
+    log.Println("pt2 Num Stones:", totStones2)
 }
