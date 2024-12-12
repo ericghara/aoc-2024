700123d29c72230903d14913075e351226db829e
ericghara <eric.gharakhanian@gmail.com>
2024-12-11 23:07:42 -0800

    day 12 part 2

--- 12/sol_12.go
+++ 12/sol_12.go
@@ -16,43 +16,73 @@ func main() {
         seen = append(seen, make([]int, len(garden[len(seen)])))
     }
 
-    var measure func(int, int, rune, int) (p int, a int);
+    var measure func(int, int, int, rune, int) (p int, a int);
 
-    moves := [4][2]int{{0,1},{0,-1},{1,0},{-1,0}}
+    moves := [4][2]int{{0,1},{1,0},{0,-1},{-1,0}}
+    boundaries := []map[[2]int]int{{},{},{},{}} // by direction
 
-    measure = func(r, c int, plant rune, id int) (int,int) {
+    measure = func(r, c, d int, plant rune, id int) (int,int) {
         if r < 0 || r >= len(garden) || c < 0 || c >= len(garden[r]) {
+            boundaries[d][[2]int{r,c}] = id
             return 1, 0
         }
         if seen[r][c] == id {
             return 0, 0
         }
         if seen[r][c] != 0 || garden[r][c] != plant {
+            boundaries[d][[2]int{r,c}] = id
             return 1, 0
         }
         seen[r][c] = id
         p, a := 0, 1
-        for _, move := range moves {
-            dP, dA := measure(r+move[0], c+move[1], plant, id);
+        for nD, move := range moves {
+            dP, dA := measure(r+move[0], c+move[1], nD, plant, id);
             p += dP
             a += dA
         }
         return p, a
     }
 
-    cost := 0
+    countSides := func() int {
+        sides := 0
+        for i := range len(boundaries) {
+            for k, id := range boundaries[i] {
+                sides++
+                dir, start := moves[(i+1)%4], k
+                for boundaries[i][start] == id {
+                    start[0]+=dir[0]
+                    start[1]+=dir[1]
+                }
+                dir[0] *= -1
+                dir[1] *= -1
+                start[0] += dir[0]
+                start[1] += dir[1]
+                for boundaries[i][start] == id {
+                    delete(boundaries[i], start)
+                    start[0] += dir[0]
+                    start[1] += dir[1]
+                }
+            }
+        }
+        return sides
+    }
+
+    fullCost, discountCost := 0, 0
 
     for r := range len(garden) {
         for c := range len(garden[r]) {
             if seen[r][c] != 0 {
                 continue
             }
-            id := r*len(garden[r])+c
+            id := r*len(garden[r])+c+1
             plant := garden[r][c]
-            p, a := measure(r,c,plant, id)
-            cost += p * a
+            p, a := measure(r,c,-1,plant, id)
+            fullCost += p * a
+            s := countSides()
+            discountCost += s * a
         }
     }
 
-    log.Println("Fence cost: ", cost)
+    log.Println("Full cost:", fullCost)
+    log.Println("Discount cost:", discountCost)
 }
