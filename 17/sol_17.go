1a041a1a6df2848e4bf345456ed7d5c859ea3672
ericghara <eric.gharakhanian@gmail.com>
2024-12-16 23:06:37 -0800

    day 17 part 1 complete

---
+++ 17/sol_17.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+    "aoc/parseutil"
+    "log"
+    "regexp"
+    "strconv"
+    "strings"
+)
+
+var (
+    regs = [3]int64{}
+    ins = []int64{}
+    insPtr = int64(0)
+    outs = []int64{}
+)
+
+func adv(combo int64) {
+    xdv(&regs[0], &regs[0], combo)
+}
+
+func bxl(lit int64) {
+    regs[1] ^= lit
+}
+
+func bst(combo int64) {
+    cVal := comboDecode(combo)
+    regs[1] = cVal & 0b111
+}
+
+func jnz(lit int64) {
+    if regs[0] == 0 {
+        return 
+    }
+    insPtr = lit-2
+}
+
+func bxc(noop int64) {
+    regs[1] = regs[1]^regs[2]
+}
+
+func out(combo int64) {
+    cVal := comboDecode(combo)
+    outs = append(outs, cVal & 0b111)
+
+}
+
+func bdv(combo int64) {
+    xdv(&regs[0], &regs[1], combo)
+}
+
+func cdv(combo int64) {
+    xdv(&regs[0], &regs[2], combo)
+}
+
+func xdv(inReg *int64, outReg *int64, combo int64) {
+    shift := comboDecode(combo)
+    if shift > 63 {
+        log.Println("overflow")
+        *outReg = 0
+    } else {
+        *outReg = *inReg/(1 << comboDecode(combo))
+    }
+}
+
+func comboDecode(combo int64) int64 {
+    switch combo {
+    case 0,1,2,3:
+        return combo
+    case 4,5,6:
+        return regs[combo-4]
+    default:
+        log.Panic("Unrecognized operand: ", combo)
+    }
+    return -1
+}
+
+
+
+func main() {
+    sects := parseutil.ReadInputSections(`^\s*$`)
+    matcher := regexp.MustCompile(`[^:]*:\s((\d+,*)+)`)
+    for i, line := range sects[0] {
+        numStr := matcher.FindAllStringSubmatch(line, -1)
+        n, err := strconv.ParseInt(numStr[0][1], 10, 64) 
+        if err != nil {
+            log.Panic("parse error", numStr)
+        }
+        regs[i] = n
+    }
+    ins = parseutil.ParseInts(strings.Split(matcher.FindAllStringSubmatch(sects[1][0], -1)[0][1], ","))
+    log.Println(regs)
+    log.Println(ins)
+    for insPtr < int64(len(ins)) {
+        opCode := ins[insPtr]
+        operand := ins[insPtr+1]
+        var operator func(int64)
+        switch opCode {
+        case 0: operator = adv
+        case 1: operator = bxl
+        case 2: operator = bst
+        case 3: operator = jnz
+        case 4: operator = bxc
+        case 5: operator = out
+        case 6: operator = bdv
+        case 7: operator = cdv
+        default: log.Panic("Unknown opcode", opCode)
+        }
+        operator(operand)
+        insPtr+=2
+    }
+    outStrs := []string{}
+    for _, out := range outs {
+        outStr := strconv.FormatInt(out, 10)
+        outStrs = append(outStrs, outStr)
+    }
+
+    log.Println(strings.Join(outStrs, ","))
+}
