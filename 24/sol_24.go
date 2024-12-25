704b2a27e0eb9352ff3d888a787f51b3ea881090
ericghara <eric.gharakhanian@gmail.com>
2024-12-25 00:14:44 -0800

    day 24 works but rough

--- 24/sol_24.go
+++ 24/sol_24.go
@@ -5,7 +5,11 @@ import (
     "log"
     "regexp"
     "strings"
-    "sort"
+    "strconv"
+    "fmt"
+    "math/bits"
+    "math"
+    "slices"
 )
 
 func and(g* Gate) {
@@ -46,8 +50,12 @@ func (g* Gate) Known() []string {
 
 var (
     vals = map[string]int{}
-    gates = []Gate{}
+    gatePs = []*Gate{}
     edgeTo = map[string][]*Gate{}
+    numBits = 0
+    xs = []string{}
+    ys = []string{}
+    zs = []string{}
 )
 
 func main() {
@@ -65,7 +73,6 @@ func main() {
     }
     
     m1 := regexp.MustCompile(`([^\s]*)\s+(\w*)\s+([^\s]+)\s+->\s+([^\s]*)`)
-    q := []string{}
     for _, line := range sections[1] {
         match := m1.FindStringSubmatch(line)
         if len(match) == 0 {
@@ -77,50 +84,138 @@ func main() {
         case "OR": fn = or
         case "XOR": fn = xor
         default: log.Panic("invalid token", match[2])
-    }
-        gates = append(gates, 
-            Gate{
+        }
+        gatePs = append(gatePs, 
+            &Gate{
                 Inputs:[2]string{match[1], match[3]},
                 Output: match[4],
                 eval: fn,
         })
-        cur := &gates[len(gates)-1]
+        cur := gatePs[len(gatePs)-1]
         for _, reg := range [2]string{match[1], match[3]} {
             edgeTo[reg] = append(edgeTo[reg], cur)
-            q = append(q, reg)
+            fmt.Printf("cur %p edgeTo %v\n",cur, edgeTo[reg])
+        }
+        if strings.HasPrefix(cur.Output, "z") {
+            bits, err := strconv.Atoi(cur.Output[1:])
+            if err != nil {
+                log.Panic("unable to parse", cur.Output)
+            }
+            numBits = max(numBits, int(bits+1))
         }
     }
 
-    for len(q) > 0 {
-        next := []string{}
-        for _, reg := range q {
-            for _, gate := range  edgeTo[reg] {
-                log.Println("Known:", gate.Known())
-                if len(gate.Known()) == 2 {
-                    log.Println(gate)
-                    gate.Eval()
-                    next = append(next, gate.Output)
+    for i := range numBits {
+        xs = append(xs, fmt.Sprintf("x%02d", i))
+        ys = append(ys, fmt.Sprintf("y%02d", i))
+        zs = append(zs, fmt.Sprintf("z%02d", i))
+    }
+
+    sum := func(x, y int) int {
+        vals = map[string]int{}
+        q := []string{}
+        for i := range numBits {
+            vals[xs[i]] = x&1
+            vals[ys[i]] = y&1
+            x >>= 1
+            y >>= 1
+            q = append(q, xs[i], ys[i])
+        }
+
+        for len(q) > 0 {
+            next := []string{}
+            for _, reg := range q {
+                //log.Println(reg)
+                for _, gate := range edgeTo[reg] {
+                    if len(gate.Known()) == 2 {
+                        gate.Eval()
+                        //log.Println(gate.Output)
+                        next = append(next, gate.Output)
+                    }
+                }
+            }
+            q = next
+        }
+        var tot int
+        for i, zStr := range zs {
+            tot |= (vals[zStr]  << i)
+        }
+        return tot 
+    }
+
+    var test func(uint) uint
+    test = func(bad uint) uint {
+        bitMask := uint((1 << numBits) - 1)
+        newBad := uint(sum(0,0))
+        for i := range numBits-1 { // test me
+            mask := uint(1 << i)
+            if mask & bad == 0 || newBad & mask > 0 {
+                continue
+            }
+            beforeMask := uint(mask >> 1)
+            adds := [][2]uint{{mask,mask},{0,mask},{mask,0}, {mask|beforeMask,beforeMask}}
+            for _, nums := range adds {
+                if found := sum(int(nums[0]), int(nums[1])); (nums[0] + nums[1]) & bitMask != uint(found) {
+                   //log.Println(nums[0], nums[1], found, (nums[0]+nums[1])&bitMask )
+                    newBad |= mask
+                    break
+                    //log.Println(i)
                 }
             }
         }
-        q = next
-        log.Println("step")
-    }
-
-    log.Println(vals)
-    zees := []string{}
-    for key, _ := range vals {
-        if strings.HasPrefix(key, "z") {
-            zees = append(zees, key)
+        if bits.OnesCount(newBad) < bits.OnesCount(bad) && bad != math.MaxUint {
+            return test(math.MaxUint)
         }
-    }
-    sort.Sort(sort.Reverse(sort.StringSlice(zees)))
-    var part1 int
-    for i := range len(zees) {
-        val, _ := vals[zees[i]]
-        part1 <<= 1
-        part1 |= val
+        return newBad
     }
 
-    log.Println("Part 1", part1)
-}
+
+    var bruteForce func(i, swap int, bad uint) []*Gate
+
+    bruteForce = func(i, swaps int, bad uint) []*Gate {
+        if bad == 0 {
+            return []*Gate{}
+        }
+        if swaps == 4 || i >= len(gatePs) {
+            return nil
+        }
+        if ret := bruteForce(i+1, swaps, bad); ret != nil {
+            return ret
+        }
+        if gatePs[i] == nil {
+            return nil
+        }
+        for j := i+1; j < len(gatePs); j++ {
+            if gatePs[j] == nil {
+                continue
+            }
+            gatePs[i].Output, gatePs[j].Output = gatePs[j].Output, gatePs[i].Output
+            tmp := gatePs[j]
+            gatePs[j] = nil
+            newBad := test(bad)
+            if bits.OnesCount(newBad) < bits.OnesCount(bad) {
+                log.Println(swaps, newBad, tmp.Output, gatePs[i].Output)
+                if ret := bruteForce(i+1, swaps+1, newBad); ret != nil {
+                    return append(ret, gatePs[i], tmp)
+                }
+            }
+            gatePs[j] = tmp
+            gatePs[i].Output, gatePs[j].Output = gatePs[j].Output, gatePs[i].Output
+        }
+        return nil
+    }
+
+    var x, y int
+    for i := range numBits {
+        x |= (vals[xs[i]] << i)
+        y |= (vals[ys[i]] << i)
+    }
+    
+    log.Println("Part 1", sum(x, y))
+    bad := test(0xffffffffffffffff)
+    log.Println("start", bad)
+    log.Println("test", bits.OnesCount(bad))
+    for _, gate := range bruteForce(0, 0, test(bad)) {
+        log.Println(gate.Output)
+    }
+} 
