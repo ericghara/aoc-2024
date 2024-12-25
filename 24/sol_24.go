bf6de3b5f7393f3861c0d84f82c0a4da1c8eaf27
ericghara <eric.gharakhanian@gmail.com>
2024-12-25 02:13:26 -0800

    24 part2 parallel

--- 24/sol_24.go
+++ 24/sol_24.go
@@ -8,36 +8,35 @@ import (
     "strconv"
     "fmt"
     "math/bits"
-    "math"
-    "slices"
+    "sort"
 )
 
-func and(g* Gate) {
-    inputs := g.Known()
+func and(g* Gate, vals map[string]int) {
+    inputs := g.Known(vals)
     vals[g.Output] = vals[inputs[0]] & vals[inputs[1]]
 }
 
-func or(g* Gate) {
-    inputs := g.Known()
+func or(g* Gate, vals map[string]int) {
+    inputs := g.Known(vals)
     vals[g.Output] = vals[inputs[0]] | vals[inputs[1]]
 }
 
-func xor(g* Gate) {
-    inputs := g.Known()
+func xor(g* Gate, vals map[string]int) {
+    inputs := g.Known(vals)
     vals[g.Output] = vals[inputs[0]] ^ vals[inputs[1]]
 } 
 
 type Gate struct {
     Inputs [2]string
     Output string
-    eval func(g* Gate)
+    eval func(g* Gate, vals map[string]int)
 }
 
-func (g* Gate) Eval() {
-    g.eval(g)
+func (g* Gate) Eval(vals map[string]int) {
+    g.eval(g, vals)
 }
 
-func (g* Gate) Known() []string {
+func (g* Gate) Known(vals map[string]int) []string {
     ret := []string{}
     for _, input := range g.Inputs {
         _, ok := vals[input]
@@ -49,7 +48,6 @@ func (g* Gate) Known() []string {
 }
 
 var (
-    vals = map[string]int{}
     gatePs = []*Gate{}
     edgeTo = map[string][]*Gate{}
     numBits = 0
@@ -62,6 +60,7 @@ func main() {
     sections := parseutil.ReadInputSections(`^\s*$`)
 
     m0 := regexp.MustCompile(`([^:]*):\s*([01])`)
+    initVals := map[string]int{}
 
     for _, line := range sections[0] {
         match := m0.FindStringSubmatch(line)
@@ -69,7 +68,7 @@ func main() {
             log.Panic("Unable to parse line", line)
         }
         val := match[2][0] - '0'
-        vals[match[1]] = int(val)
+        initVals[match[1]] = int(val)
     }
     
     m1 := regexp.MustCompile(`([^\s]*)\s+(\w*)\s+([^\s]+)\s+->\s+([^\s]*)`)
@@ -78,7 +77,7 @@ func main() {
         if len(match) == 0 {
             log.Panic("Unable to parse line", line)
         }
-        var fn func(*Gate)
+        var fn func(*Gate, map[string]int)
         switch match[2] {
         case "AND": fn = and
         case "OR": fn = or
@@ -94,7 +93,6 @@ func main() {
         cur := gatePs[len(gatePs)-1]
         for _, reg := range [2]string{match[1], match[3]} {
             edgeTo[reg] = append(edgeTo[reg], cur)
-            fmt.Printf("cur %p edgeTo %v\n",cur, edgeTo[reg])
         }
         if strings.HasPrefix(cur.Output, "z") {
             bits, err := strconv.Atoi(cur.Output[1:])
@@ -112,7 +110,7 @@ func main() {
     }
 
     sum := func(x, y int) int {
-        vals = map[string]int{}
+        vals := map[string]int{}
         q := []string{}
         for i := range numBits {
             vals[xs[i]] = x&1
@@ -125,11 +123,9 @@ func main() {
         for len(q) > 0 {
             next := []string{}
             for _, reg := range q {
-                //log.Println(reg)
                 for _, gate := range edgeTo[reg] {
-                    if len(gate.Known()) == 2 {
-                        gate.Eval()
-                        //log.Println(gate.Output)
+                    if len(gate.Known(vals)) == 2 {
+                        gate.Eval(vals)
                         next = append(next, gate.Output)
                     }
                 }
@@ -144,28 +140,47 @@ func main() {
     }
 
     var test func(uint) uint
+    numThread := 14
+    in, out := make(chan [2]uint, numThread), make(chan uint, numThread)
+    defer close(in)
+    defer close(out)
+
+    for i := 0; i < numThread; i++ {
+        go func() {
+            for nums := range in {
+                res := sum(int(nums[0]), int(nums[1]))
+                out <- (nums[0]+nums[1]) ^ uint(res)
+            }
+        }()
+    }
+
     test = func(bad uint) uint {
-        bitMask := uint((1 << numBits) - 1)
-        newBad := uint(sum(0,0))
-        for i := range numBits-1 { // test me
+        jobs := [][2]uint{{0,0}} 
+        for i := range numBits-1 {
             mask := uint(1 << i)
-            if mask & bad == 0 || newBad & mask > 0 {
+            if mask & bad == 0 {
                 continue
             }
             beforeMask := uint(mask >> 1)
             adds := [][2]uint{{mask,mask},{0,mask},{mask,0}, {mask|beforeMask,beforeMask}}
             for _, nums := range adds {
-                if found := sum(int(nums[0]), int(nums[1])); (nums[0] + nums[1]) & bitMask != uint(found) {
-                   //log.Println(nums[0], nums[1], found, (nums[0]+nums[1])&bitMask )
-                    newBad |= mask
-                    break
-                    //log.Println(i)
-                }
+                jobs = append(jobs, nums)
             }
         }
-        if bits.OnesCount(newBad) < bits.OnesCount(bad) && bad != math.MaxUint {
-            return test(math.MaxUint)
+            
+            
+        go func() {
+            for _, j := range jobs {
+                in <- j
+            } 
+        }()
+
+        bitMask := uint((1 << numBits) - 1)
+        var newBad uint
+        for i := 0; i < len(jobs); i++ {
+            newBad |= <-out
         }
+        newBad &= bitMask
         return newBad
     }
 
@@ -207,15 +222,19 @@ func main() {
 
     var x, y int
     for i := range numBits {
-        x |= (vals[xs[i]] << i)
-        y |= (vals[ys[i]] << i)
+        x |= (initVals[xs[i]] << i)
+        y |= (initVals[ys[i]] << i)
     }
     
     log.Println("Part 1", sum(x, y))
+    log.Println(numBits)
     bad := test(0xffffffffffffffff)
     log.Println("start", bad)
-    log.Println("test", bits.OnesCount(bad))
+    log.Println("bad bits", bits.OnesCount(bad))
+    part2 := []string{}
     for _, gate := range bruteForce(0, 0, test(bad)) {
-        log.Println(gate.Output)
+        part2 = append(part2, gate.Output)
     }
-} 
+    sort.Strings(part2)
+    log.Println("Part 2: ", strings.Join(part2, ","))
+}
